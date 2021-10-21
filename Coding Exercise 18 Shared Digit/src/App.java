3d0bbb57687832164e19c42b68ad28ce68c08b47
Priyansu1 <priyansusahoo1@gmail.com>
2021-10-21 22:46:29 +0530

    Updated Project: Coding Challenge 18: Shared Digit

--- Coding Exercise 18 Shared Digit/src/App.java
+++ Coding Exercise 18 Shared Digit/src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //System.out.println("Hello, World!");
+        SharedDigit.hasSharedDigit(12, 23);
+        SharedDigit.hasSharedDigit(9, 99);
+        SharedDigit.hasSharedDigit(15, 55);
     }
 }
--- Coding Exercise 18 Shared Digit/src/SharedDigit.java
+++ Coding Exercise 18 Shared Digit/src/SharedDigit.java
@@ -1,3 +1,15 @@
-public class SharedDigit {
+public class SharedDigit 
+{
     
+        public static boolean hasSharedDigit(int x,int y) 
+        {
+            if ((x < 10 || x > 99) || (y < 10 || y > 99)) 
+            {
+                System.out.println("false");
+                return false;
+            }
+            System.out.println("true");
+            return ((x / 10 == y / 10) || (x / 10 == y % 10) || (y / 10 == x % 10) || (x % 10 == y % 10));
+    	} 
+    	
 }
