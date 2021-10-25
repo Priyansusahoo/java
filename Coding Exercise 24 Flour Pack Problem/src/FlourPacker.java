501fa13b8efab6d8e6f842ff1bfb529f80b95e0d
Priyansu Sahoo <priyansusahoo1@gmail.com>
2021-10-25 13:05:35 +0530

    AddedProject : Coding Exercise 24 FlourPackProblem

---
+++ Coding Exercise 24 Flour Pack Problem/src/FlourPacker.java
@@ -0,0 +1,12 @@
+public class FlourPacker {
+    // write your code here
+        public static boolean canPack(int bigCount, int smallCount, int goal){
+        if(bigCount < 0 || smallCount < 0 || goal < 0) {
+            return false;
+        }
+        if(bigCount*5 + smallCount < goal) { 
+            return false;
+        }
+        return (goal%5 <= smallCount);
+    }
+}
