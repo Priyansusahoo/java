460c50da49d758554907c969044ec3f091c9e743
Priyansu Sahoo <priyansusahoo1@gmail.com>
2021-10-25 22:45:42 +0530

    Added Project : ReadingUserInputCHALLENGE

---
+++ ReadingUserInputCHALLENGE/src/Main.java
@@ -0,0 +1,31 @@
+// import java.util.Scanner;
+
+// public class Main {
+
+// public static void main(String[] args) {
+
+// Scanner input = new Scanner(System.in);
+
+// System.out.println("Total inputs: ");
+
+// boolean counter = input.hasNextInt();
+
+// if (counter) {
+
+// int number = 0;
+// int sum = 0;
+// while (number <= ) {
+
+// int inputNumber = 0;
+// System.out.println("Enter number #" + number);
+// inputNumber = input.nextInt();
+// number++;
+// sum += inputNumber;
+// }
+// System.out.println("Sum of all the numbers is " + sum);
+// } else {
+// System.out.println("Invalid Number");
+// }
+// input.close();
+// }
+// }
---
+++ ReadingUserInputCHALLENGE/src/TIM.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class TIM {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int counter = 0;
+        int sum = 0;
+
+        while (true) {
+
+            int order = counter + 1;
+            System.out.println("Enter number #" + order + ":");
+
+            boolean isAnInt = scanner.hasNextInt();
+
+            if (isAnInt) {
+                int number = scanner.nextInt();
+                counter++;
+                sum += number;
+                if (counter == 10) {
+                    break;
+                }
+            } else {
+                System.out.println("Invalid Number");
+            }
+            scanner.nextLine(); // ekjfbhvcsertuvbvbujhvbubvdubvudbh
+        }
+        System.out.println("sum = " + sum);
+        scanner.close();
+    }
+}
