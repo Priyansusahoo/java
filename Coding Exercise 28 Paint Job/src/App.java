082b6bc4abc426bc1a74e80523d90f3137279f12
Priyansu Sahoo <priyansusahoo1@gmail.com>
2021-10-26 20:41:22 +0530

    Minor Changes in Coding Exercise 28 Paint Job

--- Coding Exercise 28 Paint Job/src/App.java
+++ Coding Exercise 28 Paint Job/src/App.java
@@ -5,13 +5,13 @@ public class App {
         PaintJob.getBucketCount(6.26, 2.2);
         PaintJob.getBucketCount(3.26, 0.75);
 
-        System.out.println("**************************************");
+        // System.out.println("**************************************");
 
         PaintJob.getBucketCount(-3.4, 2.1, 1.5);
         PaintJob.getBucketCount(3.4, 2.1, 1.5);
         PaintJob.getBucketCount(7.25, 4.3, 2.35);
 
-        System.out.println("**************************************");
+        // System.out.println("**************************************");
 
         PaintJob.getBucketCount(-3.4, 2.1, 1.5, 2);
         PaintJob.getBucketCount(3.4, 2.1, 1.5, 2);
--- Coding Exercise 28 Paint Job/src/PaintJob.java
+++ Coding Exercise 28 Paint Job/src/PaintJob.java
@@ -1,3 +1,6 @@
+////////////////////////////////////////////
+// Practice again
+///////////////////////////////////////////
 public class PaintJob {
     public static int getBucketCount(double width, double height, double areaPerBucket, int extraBuckets) {
         if (width <= 0 || height <= 0 || areaPerBucket <= 0 || extraBuckets < 0)
