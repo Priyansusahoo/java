f61a9c7558dccb0eea84545359d9cf86b9a4ca98
Priyansu Sahoo <priyansusahoo1@gmail.com>
2021-11-01 02:16:57 +0530

    Added project : Composition Challenge Exercise

---
+++ Section 7/Composition Challenge Exercise/src/Ceiling.java
@@ -0,0 +1,17 @@
+public class Ceiling {
+    private int height;
+    private int paintedColour;
+
+    public Ceiling(int height, int paintedColour) {
+        this.height = height;
+        this.paintedColour = paintedColour;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getPaintedColour() {
+        return paintedColour;
+    }
+}
