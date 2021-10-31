2bad54a763a664dd8aef72d56afc9f47dd9bf2c3
Priyansu Sahoo <priyansusahoo1@gmail.com>
2021-10-31 14:57:58 +0530

    Added Project : Coding Exercise 35 Cylinder

---
+++ Section 6/Coding Exercise 35 Cylinder/src/Cylinder.java
@@ -0,0 +1,25 @@
+import java.awt.geom.Area;
+
+public class Cylinder extends Circle{
+
+    private double height;
+
+    public Cylinder(double radius, double height) {
+        super(radius);
+        if (height < 0) {
+            this.height = 0;
+        } else {
+            this.height = height;
+        }
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+
+    public double getVolume() {
+        return (getArea() * getHeight());
+    }
+
+}
