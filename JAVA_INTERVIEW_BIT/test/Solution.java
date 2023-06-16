6f350e773473fc1bf20f98a67f2d4b17e188cc99
Priyansu Sahoo <priyansusahoo1@gmail.com>
2023-06-16 12:58:23 +0000

    added : Integer Object -> test

---
+++ JAVA_INTERVIEW_BIT/test/Solution.java
@@ -0,0 +1,8 @@
+package JAVA_INTERVIEW_BIT.test;
+
+public class Solution {
+    public static void main(String[] args) {
+        Integer sum = new Integer(5);
+        System.out.println(sum);
+    }
+}
