10e208b0c5a1eab5c39c59fd7038656d5e488a7f
Priyansu Sahoo <priyansusahoo1@gmail.com>
2023-06-28 06:08:07 +0000

    added : Thread, Runnable and Lambda Experssions in Java

---
+++ ThreadAndRunnable/Demo.java
@@ -0,0 +1,30 @@
+class Demo {
+    public static void main(String[] args) {
+        Runnable obj1 = () -> {
+            for (int i = 0; i <= 5; i++) {
+                System.out.println("Hi");
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        Runnable obj2 = () -> {
+            for (int i = 0; i <= 5; i++) {
+                System.out.println("Hello");
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        Thread t1 = new Thread(obj1);
+        Thread t2 = new Thread(obj2);
+
+        t1.start();
+        t2.start();
+    }
+}
